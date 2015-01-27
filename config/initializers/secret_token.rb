afeeb289ce752f743d49220249b13ff8f57bd0ba
Albert Tseng <albert@chegg.com>
2015-01-27 13:23:45 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NoiseAlert::Application.config.secret_key_base = '85d794d79964278e8b92eb1267453f98848ec65df1dd638c75782da4e04ff814576cb8a882087b7033031beae2ba7d3c0cf1a361ae0bfe8ba80ecab4faef84e1'
